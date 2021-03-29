04eddd488f81f5398c1b0f5c1ec61797882dee10
Piotr <piotrbadelek@protonmail.com>
2021-03-29 15:39:28 +0200

    why was it here

--- assets/js/bootstrap.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-
-function $(selector, context) {
-	return context ? context.querySelector(selector) : document.querySelector(selector);
-};
-
-function $$(selector, context) {
-	return context ? context.querySelectorAll(selector) : document.querySelectorAll(selector);
-};
